036d41a87642e8a91cec5ababab936939b1fae1c
JT Smith <jt@plainblack.com>
2010-03-03 21:39:22 -0600

    With Ricardo's guidance, added aliasing for config file directives. Not working just yet.

--- lib/Dist/Zilla/Plugin/MetaNoIndex.pm
+++ lib/Dist/Zilla/Plugin/MetaNoIndex.pm
@@ -6,54 +6,57 @@ with 'Dist::Zilla::Role::MetaProvider';
 
 sub mvp_multivalue_args { qw(folder directory file namespace package) }
 
-# just here for backward compatibility
-has folder => (
-    is          => 'ro',
-    isa         => 'ArrayRef',
-    trigger     => sub {
-        my ($self, $new, $old) = @_;
-        $self->directory($new);
-    },
-);
+sub mvp_aliases {
+    return {
+        dir         => 'directories',
+        directory   => 'directories',
+        folder      => 'directories',
+        file        => 'files',
+        'package'   => 'packages',
+        module      => 'packages',
+        class       => 'packages',
+        namespace   => 'namespaces',
+    };
+}
 
-has directory => (
+has directories => (
     is          => 'rw',
     isa         => 'ArrayRef',
-    predicate   => 'has_directory',
+    predicate   => 'has_directories',
 );
 
-has file => (
+has files => (
     is          => 'ro',
     isa         => 'ArrayRef',
-    predicate   => 'has_file',
+    predicate   => 'has_files',
 );
 
-has package => (
+has packages => (
     is          => 'ro',
     isa         => 'ArrayRef',
-    predicate   => 'has_package',
+    predicate   => 'has_packages',
 );
 
-has namespace => (
+has namespaces => (
     is          => 'ro',
     isa         => 'ArrayRef',
-    predicate   => 'has_namespace',
+    predicate   => 'has_namespaces',
 );
 
 sub metadata {
     my $self = shift;
     my %no = ();
-    if ($self->has_directory) {
-        $no{directory} = $self->directory;
+    if ($self->has_directories) {
+        $no{directory} = $self->directories;
     }
-    if ($self->has_file) {
-        $no{file} = $self->file;
+    if ($self->has_files) {
+        $no{file} = $self->files;
     }
-    if ($self->has_package) {
-        $no{package} = $self->package;
+    if ($self->has_packages) {
+        $no{package} = $self->packages;
     }
-    if ($self->has_file) {
-        $no{namespace} = $self->namespace;
+    if ($self->has_namespaces) {
+        $no{namespace} = $self->namespaces;
     }
     return { no_index => \%no };
 }
@@ -77,6 +80,30 @@ In your F<dist.ini>:
 =head1 DESCRIPTION
 
 This plugin allows you to prevent PAUSE/CPAN from indexing files you don't want indexed. This is useful if you build test classes or example classes that are used for those purposes only, and are not part of the distribution. It does this by adding a C<no_index> block to your F<META.yml> file in your distribution.
+
+The following directives are available.
+
+=over
+
+=item directory
+
+Exclude folders and everything in them. Example: C<author.t>. Aliases: C<folder>, C<dir>.
+
+=item file
+
+Exclude a specific file. Example: C<lib/Foo.pm>.
+
+=item package
+
+Exclude by package name. Example: C<My::Package>. Aliases: C<class>, C<module>.
+
+=item namespace
+
+Exclude everything under a specific namespace. Example: C<My::Package>. 
+
+B<NOTE:> This will not exclude the package C<My::Package>, only everything under it like C<My::Package::Foo>.
+
+=back
 
 =head1 SUPPORT
 
