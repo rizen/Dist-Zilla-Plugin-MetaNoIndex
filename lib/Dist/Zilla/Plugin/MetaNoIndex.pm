ce95d0a9995bb77955dbe1a8e7fe2b2fca17ee35
JT Smith <jt@plainblack.com>
2010-03-03 21:08:15 -0600

    - Added support for 'package', 'file', and 'namespace'. - Changed 'folder' to 'directory', but kept 'folder' as a deprecated alias for backward compatibility.

--- lib/Dist/Zilla/Plugin/MetaNoIndex.pm
+++ lib/Dist/Zilla/Plugin/MetaNoIndex.pm
@@ -4,17 +4,58 @@ use Moose;
 use Moose::Autobox;
 with 'Dist::Zilla::Role::MetaProvider';
 
-sub mvp_multivalue_args { qw(folder) }
+sub mvp_multivalue_args { qw(folder directory file namespace package) }
 
+# just here for backward compatibility
 has folder => (
     is          => 'ro',
     isa         => 'ArrayRef',
-    required    => 1,
+    trigger     => sub {
+        my ($self, $new, $old) = @_;
+        $self->directory($new);
+    },
+);
+
+has directory => (
+    is          => 'rw',
+    isa         => 'ArrayRef',
+    predicate   => 'has_directory',
+);
+
+has file => (
+    is          => 'ro',
+    isa         => 'ArrayRef',
+    predicate   => 'has_file',
+);
+
+has package => (
+    is          => 'ro',
+    isa         => 'ArrayRef',
+    predicate   => 'has_package',
+);
+
+has namespace => (
+    is          => 'ro',
+    isa         => 'ArrayRef',
+    predicate   => 'has_namespace',
 );
 
 sub metadata {
     my $self = shift;
-    return { no_index => { directory => $self->folder } };
+    my %no = ();
+    if ($self->has_directory) {
+        $no{directory} = $self->directory;
+    }
+    if ($self->has_file) {
+        $no{file} = $self->file;
+    }
+    if ($self->has_package) {
+        $no{package} = $self->package;
+    }
+    if ($self->has_file) {
+        $no{namespace} = $self->namespace;
+    }
+    return { no_index => \%no };
 }
 
 
@@ -27,12 +68,33 @@ Dist::Zilla::Plugin::MetaNoIndex - Stop CPAN from indexing stuff
 In your F<dist.ini>:
 
  [MetaNoIndex]
- folder = author.t
- folder = examples
+ directory = author.t
+ directory = examples
+ file = lib/Foo.pm
+ package = My::Module
+ namespace = My::Module
 
 =head1 DESCRIPTION
 
 This plugin allows you to prevent PAUSE/CPAN from indexing files you don't want indexed. This is useful if you build test classes or example classes that are used for those purposes only, and are not part of the distribution. It does this by adding a C<no_index> block to your F<META.yml> file in your distribution.
+
+=head1 SUPPORT
+
+=over
+
+=item Repository
+
+L<http://github.com/rizen/Dist-Zilla-Plugin-MetaNoIndex>
+
+=item Bug Reports
+
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Plugin-MetaNoIndex>
+
+=back
+
+=head1 SEE ALSO
+
+L<Dist::Zilla>
 
 =head1 AUTHOR
 
